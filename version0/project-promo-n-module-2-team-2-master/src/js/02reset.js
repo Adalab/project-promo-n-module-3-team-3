1cfa37b255cfd6507c36457f3d678d3c697b4be7
monipeny <mlamasrial@gmail.com>
2021-09-17 13:12:39 +0200

    download project-2-team-2

---
+++ version0/project-promo-n-module-2-team-2-master/src/js/02reset.js
@@ -0,0 +1,7 @@
+const resetFormButton = document.querySelector('.js_resetbutton');
+function handleResetLocalStorage(){
+  localStorage.removeItem('dataUser');
+  location.reload();
+}
+resetFormButton.addEventListener('click', handleResetLocalStorage);
+

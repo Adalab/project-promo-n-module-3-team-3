5b566bacc18c5e8005ab1723bea50bb8d6929e9d
monipeny <mlamasrial@gmail.com>
2021-09-27 17:15:57 +0200

    delete js class

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -6,37 +6,37 @@ function Preview() {
     <div className="page">
       <main className="main_grid">
         <section className="main__cards">
-          <button className="main__cards--button js_resetbutton">
+          <button className="main__cards--button">
             <i className="far fa-trash-alt"></i> Reset
           </button>
-          <section className="main__cards--section js-preview palette5">
+          <section className="main__cards--section palette1">
             <div className="grid-sidebar">
               <div className="main__cards--section__sidebar"></div>
-              <h1 className="main__cards--section__title1 js_previewtext js_title">
+              <h1 className="main__cards--section__title1">
                 {data.name === "" ? "Nombre Apellido" : data.name}
               </h1>
-              <h2 className="main__cards--section__title2 js_previewtext js_job">
+              <h2 className="main__cards--section__title2">
                 {data.job === "" ? "Front-end developer" : data.job}
               </h2>
             </div>
-            <div className="main__cards--section__img profile__image js__profile-image"></div>
+            <div className="main__cards--section__img profile__image"></div>
             <nav>
               <ul className="main__cards--section__links">
                 {/* <Icons /> */}
                 <li>
                   <a
-                    className="main__cards--section__links--item js-icon-circle"
+                    className="main__cards--section__links--item"
                     id="mobile_link"
                     href={
                       data.phone === "" ? "555-555-555" : `tel: ${data.phone}`
                     }
                   >
-                    <i className="fas fa-mobile-alt js-icon"></i>
+                    <i className="fas fa-mobile-alt"></i>
                   </a>
                 </li>
                 <li>
                   <a
-                    className="main__cards--section__links--item js-icon-circle"
+                    className="main__cards--section__links--item"
                     target="blank"
                     id="mail_link"
                     href={
@@ -45,12 +45,12 @@ function Preview() {
                         : `mailto: ${data.email}`
                     }
                   >
-                    <i className="far fa-envelope js-icon"></i>
+                    <i className="far fa-envelope"></i>
                   </a>
                 </li>
                 <li>
                   <a
-                    className="main__cards--section__links--item js-icon-circle"
+                    className="main__cards--section__links--item"
                     id="linkedin_link"
                     target="blank"
                     href={
@@ -59,12 +59,12 @@ function Preview() {
                         : data.linkedin
                     }
                   >
-                    <i className="fab fa-linkedin-in js-icon"></i>
+                    <i className="fab fa-linkedin-in"></i>
                   </a>
                 </li>
                 <li>
                   <a
-                    className="main__cards--section__links--item js-icon-circle"
+                    className="main__cards--section__links--item"
                     id="github_link"
                     target="blank"
                     href={
@@ -73,7 +73,7 @@ function Preview() {
                         : `https://github.com/ ${data.github}`
                     }
                   >
-                    <i className="fab fa-github-alt js-icon"></i>
+                    <i className="fab fa-github-alt"></i>
                   </a>
                 </li>
               </ul>

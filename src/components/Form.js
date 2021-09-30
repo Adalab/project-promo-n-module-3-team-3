1d75ff27227a814961435a55c12e5b9ed9136633
Paloma <paloma.arevalog@gmail.com>
2021-09-30 10:22:21 +0200

    arrows fixed and share hidden class added

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,89 +5,85 @@ import FormFill from './FormFill';
 import { useState } from 'react';
 
 const Form = (props) => {
-  //pendiente coger los props de App.js
-  const handleSubmit = (ev) => {
-    ev.preventDefault();
-  };
-  const [collapsableDesign, setCollapsableDesign] = useState('');
-  const [collapsableFill, setCollapsableFill] = useState('hidden');
-  const [collapsableShare, setCollapsableShare] = useState('hidden');
-  const [arrow, setArrow] = useState('fa-chevron-up');
-  const [arrow2, setArrow2] = useState('fa-chevron-down');
-  const [arrow3, setArrow3] = useState('fa-chevron-down');
+	//pendiente coger los props de App.js
+	const handleSubmit = (ev) => {
+		ev.preventDefault();
+	};
+	const [collapsableDesign, setCollapsableDesign] = useState('');
+	const [collapsableFill, setCollapsableFill] = useState('hidden');
+	const [collapsableShare, setCollapsableShare] = useState('hidden');
+	const [arrow, setArrow] = useState('fa-chevron-up');
+	const [arrow2, setArrow2] = useState('fa-chevron-down');
+	const [arrow3, setArrow3] = useState('fa-chevron-down');
 
-  const handlerCollapsableDesign = () => {
-    
-    if (collapsableDesign === 'hidden') {
-      setCollapsableDesign('');
-      setArrow('fa-chevron-up');
-    } else {
-      setCollapsableDesign('hidden');
-      setArrow('fa-chevron-down');
-      setCollapsableFill('');
-      setArrow2('fa-chevron-up');
-    }
+	const handlerCollapsableDesign = () => {
+		if (collapsableDesign === 'hidden') {
+			setCollapsableDesign('');
+			setArrow('fa-chevron-up');
+		} else {
+			setCollapsableDesign('hidden');
+			setArrow('fa-chevron-down');
+			setCollapsableFill('');
+			setArrow2('fa-chevron-up');
+		}
 
-    setCollapsableShare('hidden');
-    setArrow3('fa-chevron-down');
-  };
+		setCollapsableShare('hidden');
+		setArrow3('fa-chevron-down');
+	};
 
-  const handlerCollapsableFill = () => {
-    
-    if (collapsableFill === 'hidden') {
-      setCollapsableFill('');
-      setArrow2('fa-chevron-up');
-    } else {
-      setCollapsableFill('hidden');
-      setArrow2('fa-chevron-down');
-      setCollapsableShare('');
-      setArrow3('fa-chevron-up');
-    }
-    setCollapsableDesign('hidden');
-    setArrow('fa-chevron-down');
-  };
+	const handlerCollapsableFill = () => {
+		if (collapsableFill === 'hidden') {
+			setCollapsableFill('');
+			setArrow2('fa-chevron-up');
+		} else {
+			setCollapsableFill('hidden');
+			setArrow2('fa-chevron-down');
+			setCollapsableShare('');
+			setArrow3('fa-chevron-up');
+		}
+		setCollapsableDesign('hidden');
+		setArrow('fa-chevron-down');
+	};
 
-  const handlerCollapsableShare = () => {
-   
-    if (collapsableShare === 'hidden') {
-      setCollapsableShare('');
-      setArrow3('fa-chevron-up');
-    } else {
-      setCollapsableShare('hidden');
-      setArrow3('fa-chevron-down');
-    }
-    setCollapsableDesign('hidden');
-    setCollapsableFill('hidden');
-    setArrow('fa-chevron-down');
-    setArrow2('fa-chevron-down');
-  };
+	const handlerCollapsableShare = () => {
+		if (collapsableShare === 'hidden') {
+			setCollapsableShare('');
+			setArrow3('fa-chevron-up');
+		} else {
+			setCollapsableShare('hidden');
+			setArrow3('fa-chevron-down');
+		}
+		setCollapsableDesign('hidden');
+		setCollapsableFill('hidden');
+		setArrow('fa-chevron-down');
+		setArrow2('fa-chevron-down');
+	};
 
+	return (
+		<section className="section_form">
+			<form onSubmit={handleSubmit} action="#" className="form js_form">
+				<FormDesign
+					handlerCollapsableDesign={handlerCollapsableDesign}
+					arrow={arrow}
+					collapsableDesign={collapsableDesign}
+					handleInput={props.handleInput}
+				/>
+				<FormFill
+					handlerCollapsableFill={handlerCollapsableFill}
+					arrow2={arrow2}
+					collapsableFill={collapsableFill}
+					handleInput={props.handleInput}
+					data={props.data}
+				/>
 
-  return (
-    <section className="section_form">
-      <form onSubmit={handleSubmit} action="#" className="form js_form">
-        <FormDesign
-          handlerCollapsableDesign={handlerCollapsableDesign}
-          arrow={arrow}
-          collapsableDesign={collapsableDesign}
-          handleInput={props.handleInput}
-          
-        />
-        <FormFill handlerCollapsableFill={handlerCollapsableFill}
-          arrow={arrow2}
-          collapsableFill={collapsableFill}
-          handleInput={props.handleInput}
-          data={props.data}
-          />
-
-        <FormShare
-          handlerCollapsableShare={handlerCollapsableShare}
-          arrow3={arrow3}
-          collapsableShare={collapsableShare}
-        />
-      </form>
-    </section>
-  );
+				<FormShare
+					handlerCollapsableShare={handlerCollapsableShare}
+					arrow3={arrow3}
+					collapsableShare={collapsableShare}
+				/>
+			</form>
+		</section>
+	);
 };
 
 export default Form;
--- src/components/FormShare.js
+++ src/components/FormShare.js
@@ -2,61 +2,61 @@ import '../styles/components/formShare.scss';
 import Collapsable from './Collapsable';
 
 const FormShare = (props) => {
-  
+	return (
+		<>
+			<fieldset>
+				<Collapsable
+					className="fas fa-share-alt"
+					text="Comparte"
+					handlerCollapsableShare={props.handlerCollapsableShare}
+					arrow3={props.arrow3}
+					collapsableShare={props.collapsableShare}
+				/>
 
-  
-  return (
-    <>
-      <fieldset>
-      <Collapsable className="fas fa-share-alt" text="Comparte"
-      handlerCollapsableShare={props.handlerCollapsableShare}
-      arrow3={props.arrow3}
-      collapsableShare={props.collapsableShare}/>
-        
-
-        <div className={`js_content div_content ${props.collapsableShare}`}>
-          <button className="button__card js_button_share">
-            <i className="far fa-address-card"></i> Crear tarjeta
-          </button>
-        </div>
-      </fieldset>
-      // pendiente poner dentro del form la sección de compartir
-      <section className="share__section  js_share_twitter">
-        <h2 className="share__section--title js_sharetitle"></h2>
-        <a className="share__section--link js_parrafo_share" target="blank"></a>
-        <h3 className="titlecompartir">Compartir</h3>
-        <div className="section_buttons">
-          <button className="share__section--button">
-            <a
-              className="js_linktwitter linktwitter"
-              target="blank"
-              href="https://twitter.com/"
-            >
-              <i className="fab fa-twitter"></i> Twitter
-            </a>
-          </button>
-          <button className="share__section--button">
-            <a
-              className="js_linkFacebook linktwitter"
-              target="blank"
-              href="https://es-es.facebook.com/"
-            >
-              <i className="fab fa-facebook"></i> Facebook
-            </a>
-          </button>
-          <button className="share__section--button">
-            <a
-              className="js_linkLinkedin linktwitter"
-              target="blank"
-              href="https://www.linkedin.com/in/"
-            >
-              <i className="fab fa-linkedin-in js-icon"></i> Linkedin
-            </a>
-          </button>
-        </div>
-      </section>
-    </>
-  );
+				<div
+					className={`js_content div_content  ${props.collapsableShare}`}>
+					<button className="button__card js_button_share">
+						<i className="far fa-address-card"></i> Crear tarjeta
+					</button>
+				</div>
+			</fieldset>
+			{/* // pendiente poner dentro del form la sección de compartir */}
+			<section className="share__section hidden js_share_twitter">
+				<h2 className="share__section--title js_sharetitle"></h2>
+				<a
+					className="share__section--link js_parrafo_share"
+					target="blank"></a>
+				<h3 className="titlecompartir">Compartir</h3>
+				<div className="section_buttons">
+					<button className="share__section--button">
+						<a
+							className="js_linktwitter linktwitter"
+							target="blank"
+							href="https://twitter.com/">
+							<i className="fab fa-twitter"></i> Twitter
+						</a>
+					</button>
+					<button className="share__section--button">
+						<a
+							className="js_linkFacebook linktwitter"
+							target="blank"
+							href="https://es-es.facebook.com/">
+							<i className="fab fa-facebook"></i> Facebook
+						</a>
+					</button>
+					<button className="share__section--button">
+						<a
+							className="js_linkLinkedin linktwitter"
+							target="blank"
+							href="https://www.linkedin.com/in/">
+							<i className="fab fa-linkedin-in js-icon"></i>{' '}
+							Linkedin
+						</a>
+					</button>
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default FormShare;

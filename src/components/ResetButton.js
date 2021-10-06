1aeb807bc39827ec2bd2ef306a1915c10fdc6e0f
monipeny <mlamasrial@gmail.com>
2021-10-06 20:28:06 +0200

    move code to the component ResetButton.js and import into Preview.js

---
+++ src/components/ResetButton.js
@@ -0,0 +1,9 @@
+function ResetButton() {
+  return (
+    <button className="main__cards--button">
+      <i className="far fa-trash-alt"></i> Reset
+    </button>
+  );
+}
+
+export default ResetButton;

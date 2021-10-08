3813ba615035aa10bf0fb1f8c8d5b2971969eee8
monipeny <mlamasrial@gmail.com>
2021-10-08 12:51:51 +0200

    fix link logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,19 @@
 import logo from "../images/logo-awesome-profile-cards.svg";
 import "../styles/components/header.scss";
+import { Link } from "react-router-dom";
 
 const Header = (props) => {
   return (
     <header className={props.classHeader}>
       <div>
-        <a href="http://beta.adalab.es/project-promo-n-module-2-team-2/">
+        <Link to="/">
           <img
             className="cards"
             src={logo}
             alt="Logo de awesome profile cards"
             title="Web del proyecto"
           />
-        </a>
+        </Link>
       </div>
     </header>
   );

ebcbb323cffcfcb08d91bc3ef4bad201a5279d5d
María Molina García <86959823+mariamolinagarcia@users.noreply.github.com>
2021-10-11 13:40:28 +0200

    update backend struture

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
-// Fichero src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-import App from './components/App';
+const express = require("express"); 
+const cors = require("cors"); 
 
-ReactDOM.render( <HashRouter> < App / > </HashRouter> , document.getElementById('root'));
+//Crear el servidor
+const server = express(); 
+
+//Configurar el servidor
+server.use(cors()); 
+server.use(express.json()); 
+
+//Iniciar el servidor
+const serverPort = 4000; 
+server.listen(serverPort, ()=>console.log(`Server listening at http://localhost:${serverPort}`)); 
+
+// API CARD
+
+server.post('/card', (req, res) =>{
+    console.log(req.body.palette);
+    if(req.body.name !== '' && req.body.job !== '' && req.body.phone !== '' && req.body.email !== '' && req.body.linkedin !== '' && req.body.github !== '' ){
+      res.json({
+       cardUrl: 'rutadelacarta'
+   })  
+
+    }else{
+        res.sendStatus(404);
+    }
+   });
+
+/* server.get('/card', (req, res) =>{
+return console.log('adiós');
+
+}) */
+
---
+++ web/src/services/callToApi.js
@@ -0,0 +1,18 @@
+const ENDPOINT = '//localhost:4000/card';
+
+const callToApi = (data) => {
+  return fetch(ENDPOINT, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      return response;
+    });
+};
+
+export default callToApi;

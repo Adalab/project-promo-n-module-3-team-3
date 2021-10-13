673b658587691845709fec0518e8ed083d4d9c65
monipeny <mlamasrial@gmail.com>
2021-10-13 12:26:42 +0200

    move docs into root and change name to public

--- src/index.js
+++ src/index.js
@@ -1,33 +1,43 @@
-const express = require("express"); 
-const cors = require("cors"); 
+const express = require("express");
+const cors = require("cors");
 
 //Crear el servidor
-const server = express(); 
+const server = express();
 
 //Configurar el servidor
-server.use(cors()); 
-server.use(express.json()); 
+server.use(cors());
+server.use(express.json());
 
 //Iniciar el servidor
-const serverPort = 4000; 
-server.listen(serverPort, ()=>console.log(`Server listening at http://localhost:${serverPort}`)); 
+const serverPort = 4000;
+server.listen(serverPort, () =>
+  console.log(`Server listening at http://localhost:${serverPort}`)
+);
 
 // API CARD
 
-server.post('/card', (req, res) =>{
-    console.log(req.body.palette);
-    if(req.body.name !== '' && req.body.job !== '' && req.body.phone !== '' && req.body.email !== '' && req.body.linkedin !== '' && req.body.github !== '' ){
-      res.json({
-       cardUrl: 'rutadelacarta'
-   })  
-
-    }else{
-        res.sendStatus(404);
-    }
-   });
+server.post("/card", (req, res) => {
+  console.log(req.body.palette);
+  if (
+    req.body.name !== "" &&
+    req.body.job !== "" &&
+    req.body.phone !== "" &&
+    req.body.email !== "" &&
+    req.body.linkedin !== "" &&
+    req.body.github !== ""
+  ) {
+    res.json({
+      cardUrl: "rutadelacarta",
+    });
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 /* server.get('/card', (req, res) =>{
 return console.log('adiós');
 
 }) */
 
+const staticServerPathWeb = "./public"; // En esta carpeta ponemos los ficheros estáticos
+server.use(express.static(staticServerPathWeb));

875725a236616266259e9af941ba62ba3e130717
monipeny <mlamasrial@gmail.com>
2021-10-13 13:31:48 +0200

    run dev publish

--- src/index.js
+++ src/index.js
@@ -18,20 +18,25 @@ server.listen(serverPort, () =>
 
 server.post("/card", (req, res) => {
   console.log(req.body.palette);
-  if (
-    req.body.name !== "" &&
-    req.body.job !== "" &&
-    req.body.phone !== "" &&
-    req.body.email !== "" &&
-    req.body.linkedin !== "" &&
-    req.body.github !== ""
-  ) {
-    res.json({
-      cardUrl: "rutadelacarta",
-    });
+  const response = { success: false };
+
+  if (req.body.name === "") {
+    response.error = "Debe indicar su nombre";
+  } else if (req.body.job === "") {
+    response.error = "Debe indicar su puesto";
+  } else if (req.body.phone === "") {
+    response.error = "Debe indicar su teléfono movil";
+  } else if (req.body.email === "") {
+    response.error = "Debe indicar su email";
+  } else if (req.body.linkedin === "") {
+    response.error = "Debe indicar su nick de linkedIn";
+  } else if (req.body.github === "") {
+    response.error = "Debe indicar su nick de GitHub";
   } else {
-    res.sendStatus(404);
+    response.cardURL = "Esta la url de tu tarjeta";
+    response.success = true;
   }
+  res.json(response);
 });
 
 /* server.get('/card', (req, res) =>{
@@ -41,3 +46,14 @@ return console.log('adiós');
 
 const staticServerPathWeb = "./public"; // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathWeb));
+
+// Endpoint para gestionar los errores 404
+server.get("*", (req, res) => {
+  // Relativo a este directorio
+  const notFoundFileRelativePath = "../web/404-not-found.html";
+  const notFoundFileAbsolutePath = path.join(
+    __dirname,
+    notFoundFileRelativePath
+  );
+  res.status(404).sendFile(notFoundFileAbsolutePath);
+});

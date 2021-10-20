1fda24681b6972c4226b1702cb50ebb347b19faa
JuditAldeguer <jud.vicens@hotmail.es>
2021-10-20 15:01:28 +0200

    update photo on data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const Database = require("better-sqlite3");
-const path = require("path");
+const express = require('express');
+const cors = require('cors');
+const Database = require('better-sqlite3');
+const path = require('path');
 
 //Crear el servidor
 const server = express();
@@ -10,10 +10,10 @@ const server = express();
 server.use(cors());
 
 // motor de plantillas
-server.set("view engine", "ejs");
+server.set('view engine', 'ejs');
 
-server.use(express.json({ limit: "10mb" }));
-const db = new Database("./src/db/cards.db", {
+server.use(express.json({ limit: '10mb' }));
+const db = new Database('./src/db/cards.db', {
   // this line log in console all data base queries
   verbose: console.log,
 });
@@ -24,34 +24,36 @@ server.listen(serverPort, () =>
   console.log(`Server listening at http://localhost:${serverPort}`)
 );
 
-const staticServerPathWeb = "./public";
+const staticServerPathWeb = './public';
 // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathWeb));
 
-const staticServerStylesCss = "./src/public-css";
+const staticServerStylesCss = './src/public-css';
 // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerStylesCss));
 // API CARD
 
-server.post("/card", (req, res) => {
+server.post('/card', (req, res) => {
   console.log(req.body.palette);
   let response = { success: false };
 
-  if (req.body.name === "") {
-    response.error = "Debe indicar su nombre";
-  } else if (req.body.job === "") {
-    response.error = "Debe indicar su puesto";
-  } else if (req.body.phone === "") {
-    response.error = "Debe indicar su teléfono movil";
-  } else if (req.body.email === "") {
-    response.error = "Debe indicar su email";
-  } else if (req.body.linkedin === "") {
-    response.error = "Debe indicar su nick de linkedIn";
-  } else if (req.body.github === "") {
-    response.error = "Debe indicar su nick de GitHub";
+  if (req.body.name === '') {
+    response.error = 'Debe indicar su nombre';
+  } else if (req.body.job === '') {
+    response.error = 'Debe indicar su puesto';
+  } else if (req.body.phone === '') {
+    response.error = 'Debe indicar su teléfono movil';
+  } else if (req.body.email === '') {
+    response.error = 'Debe indicar su email';
+  } else if (req.body.linkedin === '') {
+    response.error = 'Debe indicar su nick de linkedIn';
+  } else if (req.body.github === '') {
+    response.error = 'Debe indicar su nick de GitHub';
+  } else if (req.body.photo === '') {
+    response.error = 'Debe indicar su nick de imagen';
   } else {
     const query = db.prepare(
-      "INSERT INTO card (palette,name,job,phone,photo,github,linkedin,email) VALUES (?,?,?,?,?,?,?,?)"
+      'INSERT INTO card (palette,name,job,phone,photo,github,linkedin,email) VALUES (?,?,?,?,?,?,?,?)'
     );
     const data = query.run(
       req.body.palette,
@@ -65,7 +67,7 @@ server.post("/card", (req, res) => {
     );
     console.log(data);
     response.success = true;
-    if (req.hostname === "localhost") {
+    if (req.hostname === 'localhost') {
       response.cardURL = `http://localhost:4000/card/${data.lastInsertRowid}`;
     } else {
       response.cardURL = `http://react-machines.herokuapp.com/card/${data.lastInsertRowid}`;
@@ -75,22 +77,22 @@ server.post("/card", (req, res) => {
   res.json(response);
 });
 
-server.get("/card/:id", (req, res) => {
-  const query = db.prepare("SELECT * FROM card WHERE id = ?");
+server.get('/card/:id', (req, res) => {
+  const query = db.prepare('SELECT * FROM card WHERE id = ?');
   const data = query.get(req.params.id);
   console.log(data);
 
   if (data !== undefined) {
-    res.render("card", data);
+    res.render('card', data);
   } else {
-    res.json({ error: "error undefined" });
+    res.json({ error: 'error undefined' });
   }
 });
 
 // Endpoint para gestionar los errores 404
-server.get("*", (req, res) => {
+server.get('*', (req, res) => {
   // Relativo a este directorio
-  const notFoundFileRelativePath = "../public/404-not-found.html";
+  const notFoundFileRelativePath = '../public/404-not-found.html';
   const notFoundFileAbsolutePath = path.join(
     __dirname,
     notFoundFileRelativePath
